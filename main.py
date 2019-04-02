ee2b073f705bd6128e8f31ebf91c7f7e708978c6
initdtxt <k.machard@at-underground.org>
2019-04-02 11:01:01 +0200

    read value from adc

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import time
 import argparse
+import logging
+import threading
 from digi.xbee.devices import XBeeDevice
-# from digi.xbee.io import IOLine, IOMode
+from digi.xbee.io import IOLine, IOMode
 
 BAUD_RATE = 9600
-# IOLINE_IN = IOLine.DIO1_AD1
+IOLINE_IN = IOLine.DIO1_AD1
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 
 class Commander:
@@ -13,29 +17,50 @@ class Commander:
         self.device = XBeeDevice(comPort, BAUD_RATE)
         self.network = None
         self.peers = []
+        self.run = True
+        self.tasks = []
         self.device.open()
+        logging.info("Commander address: {}".format(self.device.get_64bit_addr()))
 
     def discover_peer(self):
+        def discover_callback(peer):
+            logging.info("New peer: {}".format(peer.get_64bit_addr()))
+            self.peers.append(peer)
+
+        logging.info("Starting discovering peer")
         if self.network is None:
             self.network = self.device.get_network()
         self.network.set_discovery_timeout(15)
         self.network.clear()
 
-        self.network.add_device_discovered_callback(lambda peer: self.peers.append(peer))
+        self.network.add_device_discovered_callback(discover_callback)
         self.network.start_discovery_process()
 
         while self.network.is_discovery_running():
             time.sleep(1)
-        print("Discovered: {} peers".format(len(self.peers)))
+        logging.info("Discovered: {} peers".format(len(self.peers)))
+
+    def stop_all_task(self):
+        self.run = False
+        for task in self.tasks:
+            if task.isAlive():
+                task.join()
 
     def listen_remote_event(self):
-        def rcv_callback(msg):
-            print("[{}] {}".format(
-                msg.remote_device.get_64bit_addr(),
-                msg.data.decode()
-            ))
+        def read_adc_task(peer):
+            while True:
+                logging.info("[{}] {}".format(
+                    peer.get_64bit_addr(),
+                    peer.get_adc_value(IOLINE_IN)
+                ))
+                time.sleep(0.2)
+        for peer in self.peers:
+            peer.set_io_configuration(IOLINE_IN, IOMode.ADC)
+            self.tasks.append(threading.Thread(target=read_adc_task, args=[peer]))
+        
+        for task in self.tasks:
+            task.start()
 
-        self.device.add_data_received_callback(rcv_callback)
 
     def __del__(self):
         if self.device.is_open():
@@ -51,3 +76,7 @@ if __name__ == "__main__":
 
     commander.discover_peer()
     commander.listen_remote_event()
+
+    input()
+
+    commander.stop_all_task()
