119fb1df742afd3d8fdefc8bf22d5dc5d487f9c9
leagattesco <lea.gattesco@gmail.com>
2019-04-02 16:52:08 +0200

    fix du bug sur le bouton de la led

--- interfaceWeb/index.js
+++ interfaceWeb/index.js
@@ -18,17 +18,22 @@ window.onclick = function(event) {
   }
 }
 
+// btn pour éteindre la led
 var btn = document.querySelector('input');
-var txt = document.querySelector('p');
+var txt = document.getElementsByClassName("ledStatus")[0];
 
 btn.addEventListener('click', updateBtn);
 
-function updateBtn() {
-  if (btn.value === 'Allumer la lumière') {
-    btn.value = 'Arrêter la lumière';
-    txt.textContent = 'La lumière est allumée !';
-  } else {
-    btn.value = 'Allumer la lumière';
-    txt.textContent = 'La lumière est arrêtée.';
-  }
+function updateBtn() 
+{
+  if (btn.value === 'Allumer la lumière') 
+    {
+        btn.value = 'Arrêter la lumière';
+        txt.textContent = 'La lumière est allumée.';
+    } 
+  else 
+    {
+        btn.value = 'Allumer la lumière';
+        txt.textContent = 'La lumière est arrêtée.';
+    }
 }
