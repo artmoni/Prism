64c62fe1dd8ac3f5408faee032474e0e8e578725
nicolas <n.hamelin44120@gmail.com>
2019-04-04 11:35:42 +0200

    amelioration websocket

--- interfaceWeb/js/websocket.js
+++ interfaceWeb/js/websocket.js
@@ -1,32 +1,27 @@
 var socketPrism;
 var password = new Boolean("false");
-var light = new Boolean("false");
+var light;
 
 const url = "ws://localhost";
 
-function connect() 
-{
-    socketPrism = new WebSocket(url);
+socketPrism = new WebSocket(url);
 
-    socketPrism.onopen = function (event) 
-    {
-        socketPrism.send("Authentification"); 
-    };
+socketPrism.onopen = function(event) 
+{
+    socketPrism.send("Authentification"); 
 }
 
 function readData()
 {
-    socketPrism = new WebSocket(url);
-
     if(!password)
     {
         print("Access authorized");
-        light = "true";
-
-        socketPrism.onopen = function (event) 
-        {
-            socketPrism.send(light); 
-        };
+        light = "G";
+    }
+    else if(password)
+    {
+        print("Access denied");
+        light = "R";
     }
 }
 
